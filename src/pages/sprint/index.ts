76e20d49c779082f8067d873e9084199934c5682
Max_Nikitenok <xxx250790@gmail.com>
2022-08-18 23:08:28 +0300

    feat: add minimal functionality, timer and counter

--- src/pages/sprint/index.ts
+++ src/pages/sprint/index.ts
@@ -1,11 +1,15 @@
 import html from './html';
+import randomWord from './sprint';
 import './style.css';
+import timer from './timer';
 
 function addSprintGame() {
   const sprintGame = document.createElement('div');
   sprintGame.classList.add('sprint-game');
   sprintGame.innerHTML = html;
   document.body.appendChild(sprintGame);
+  randomWord();
+  timer();
 }
 
 export default addSprintGame;
---
+++ src/pages/sprint/sprint.ts
@@ -0,0 +1,69 @@
+const randomInteger = (min: number, max: number) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+export const randomWord = async () => {
+  const word = document.getElementById('word');
+  const translation = document.getElementById('translation');
+  const url = 'https://rslang-zankorrr-db.herokuapp.com/';
+  const rightButton = document.getElementById('right');
+  const wrongButton = document.getElementById('wrong');
+  const counter = document.getElementById('pointCounter');
+  const randomPage = randomInteger(0, 29);
+
+  const response = await fetch(`${url}words/?group=0&page=${randomPage}`, {
+    method: 'GET',
+  });
+
+  const items = await response.json();
+
+  const randomIntWord = randomInteger(0, 19);
+  const randomIntForAnswer = randomInteger(0, 19);
+
+  const answerArr = [];
+
+  answerArr.push(
+    { translateWord: items[randomIntWord].wordTranslate, id: items[randomIntWord].id },
+    { translateWord: items[randomIntForAnswer].wordTranslate, id: items[randomIntForAnswer].id },
+  );
+
+  const randomIntAnswer = randomInteger(0, 1);
+
+  //   console.log(items[randomIntWord].wordTranslate);
+  //   console.log(items[randomIntForAnswer].wordTranslate);
+  //   console.log(answerArr[randomIntAnswer].translateWord);
+
+  (word as HTMLElement).innerText = items[randomIntWord].word;
+  (word as HTMLElement).setAttribute('wordId', `${items[randomIntWord].id}`);
+  (translation as HTMLElement).innerText = answerArr[randomIntAnswer].translateWord;
+  (translation as HTMLElement).setAttribute('wordId', `${answerArr[randomIntAnswer].id}`);
+
+  const pushButtons = () => {
+    const wordId = (word as HTMLElement).getAttribute('wordId');
+    const answerId = (translation as HTMLElement).getAttribute('wordId');
+    (rightButton as HTMLElement).onclick = () => {
+      if (wordId === answerId) {
+        (counter as HTMLElement).innerText = String(+((counter as HTMLElement)
+          .innerText) + 10);
+        randomWord();
+      } else {
+        randomWord();
+      }
+    };
+
+    (wrongButton as HTMLElement).onclick = () => {
+      if (wordId !== answerId) {
+        (counter as HTMLElement).innerText = String(+((counter as HTMLElement)
+          .innerText) + 10);
+        randomWord();
+      } else {
+        randomWord();
+      }
+    };
+  };
+
+  pushButtons();
+};
+
+export default randomWord;
