a57c40543729cc2bac039b5928e6cf9882afc940
Zankorrr <zankor92@gmail.com>
2022-08-18 16:35:20 +0300

    refactor: add copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,42 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const EslintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.ts'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            { 
-                test: /\.ts$/i,
-                use: ['ts-loader'],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-        new EslintPlugin({ extensions: 'ts' }),
+  entry: {
+    main: path.resolve(__dirname, './src/index.ts'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: ['ts-loader'],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, './src/assets'), to: 'assets' },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: 'ts' }),
+  ],
 };
