3610381f30363ca39bad637980daab0e2bc7f134
Max_Nikitenok <xxx250790@gmail.com>
2022-09-04 03:34:36 +0300

    style: add close button for sprint

--- src/pages/sprint/timer/timer.ts
+++ src/pages/sprint/timer/timer.ts
@@ -3,10 +3,16 @@ import './style.css';
 
 export const timer = () => {
   const loadScreenMask = document.querySelector('.load-screen');
+  const closeSprint = document.querySelector('.close-sprint') as HTMLElement;
+
   (loadScreenMask as HTMLElement).style.display = 'none';
-  setTimeout(() => {
+  const timerId = setTimeout(() => {
     showStatisticsWindow();
   }, 30000);
+
+  closeSprint.addEventListener('click', () => {
+    clearTimeout(timerId);
+  });
 };
 
 export default timer;
