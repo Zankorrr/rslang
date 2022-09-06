78652b1c0fcc59ab0723856906ce4baac178210d
Dmitry Tsimoshchanka <36646929+dimtim1992@users.noreply.github.com>
2022-09-06 15:04:35 +0300

    fix: change statistics page

--- src/pages/statistics/modules/openStatistics.ts
+++ src/pages/statistics/modules/openStatistics.ts
@@ -3,6 +3,11 @@ import { renderStatistics } from './renderStatistics';
 
 export async function openStatistics() {
   if (localStorage.getItem('userToken') && localStorage.getItem('userId')) {
+    const statisticsContainer = document.querySelector('.statistics-page');
+    if (statisticsContainer) {
+      statisticsContainer.innerHTML = '';
+    }
+
     const result = await getUserStatistics(localStorage.getItem('userId'), localStorage.getItem('userToken'));
     renderStatistics(result);
   }
